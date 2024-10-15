fe5134a4e414902199a534822ca157ba6197e0c5
Javier A Mora gil <j4viermora@gmail.com>
2024-10-14 22:31:19 -0300

    fix (backend): fix enviroment variable

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -8,8 +8,8 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-const frontendOne = process.env.FRONTEND_ONE || "http://localhost:3001";
-const frontendTwo = process.env.FRONTEND_TWO || "http://localhost:3002";
+const frontendOne = process.env.FRONTEND_ONE
+const frontendTwo = process.env.FRONTEND_TWO
 
 app.use(cors());
 
@@ -38,18 +38,16 @@ const svelteProxy = createProxyMiddleware({
   }
 });
 
+app.get("/check", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
 
 app.use('/v2', svelteProxy);
 app.use('/', vueProxy);
 
 app.use((err:any, req: Request, res: Response, next: any) => {
-    console.error(err.stack);
     res.status(500).send('Algo salió mal!');
   });
-
-app.get("/check", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
